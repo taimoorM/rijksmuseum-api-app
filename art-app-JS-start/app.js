c82a182734f8bf62a3ad57d85f9ea0bfbada8c0a
Taimoor <taimoor.m89@gmail.com>
2022-07-18 12:13:05 -0400

    added fetch method for api

--- art-app-JS-start/app.js
+++ art-app-JS-start/app.js
@@ -3,8 +3,26 @@
 const artApp = {};
 artApp.key = "zGRPvXMg";
 
+artApp.getArt = function () {
+  const url = new URL("https://www.rijksmuseum.nl/api/en/collection");
+  url.search = new URLSearchParams({
+    key: artApp.key,
+    imgonly: true,
+    q: "monkeys",
+  });
+
+  fetch(url)
+    .then((apiData) => apiData.json())
+    .then((jsonData) => {
+      //display the art on the page
+      artApp.displayArt(jsonData.artObject);
+    });
+};
+
+artApp.displayArt = function (arrayOfObjects) {};
+
 artApp.init = function () {
-  console.log("initialize...");
+  artApp.getArt();
 };
 
 artApp.init();
