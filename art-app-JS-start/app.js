e79e709867db89b9796c2ae212e622e23c3ec23f
Taimoor <taimoor.m89@gmail.com>
2022-07-18 12:51:07 -0400

    added method to display art using api data

--- art-app-JS-start/app.js
+++ art-app-JS-start/app.js
@@ -15,11 +15,33 @@ artApp.getArt = function () {
     .then((apiData) => apiData.json())
     .then((jsonData) => {
       //display the art on the page
-      artApp.displayArt(jsonData.artObject);
+      console.log(jsonData);
+      artApp.displayArt(jsonData.artObjects);
     });
 };
 
-artApp.displayArt = function (arrayOfObjects) {};
+artApp.displayArt = function (arrayOfObjects) {
+  arrayOfObjects.forEach((art) => {
+    //construct some HTML
+    const title = document.createElement("h2");
+    title.innerText = art.title;
+
+    const artist = document.createElement("p");
+    artist.innerText = art.principalOrFirstMaker;
+
+    const image = document.createElement("img");
+    image.src = art.webImage.url;
+    image.alt = art.title;
+
+    const artPiece = document.createElement("div");
+    artPiece.classList.add("piece");
+
+    //add the individual elements to artPiece
+    artPiece.append(title, artist, image);
+
+    document.querySelector("#artwork").appendChild(artPiece);
+  });
+};
 
 artApp.init = function () {
   artApp.getArt();
